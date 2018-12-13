e344156a0302c2ceed0eeff2cf942468dfb2b110
Sunny Dewangan <sadewa@microsoft.com>
2018-12-13 15:52:37 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _result = HttpGet("https://functiondeleteblobdev-mssupport.azurewebsites.net/api/HttpTrigger1");
+
+            Console.WriteLine(_result);
+            Console.ReadLine();
+        }
+        public static string HttpGet(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            X509Certificate cert = new X509Certificate(@"C:\Users\sadewa\Desktop\FunctionDeleteBlobDEV\FunctionDeleteBlobDEV-Cert.pfx", "123456");
+            request.ClientCertificates.Add(cert);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            return sr.ReadToEnd();
+        }
+    }
+}
